bfce98aea577146b6b507458514b15eaf0ec492e
Sokos6 <will.sokolowski@gmail.com>
2017-02-24 17:59:56 -0500

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-
-
 @NgModule({
-  imports:      [ 
+  imports: [
     BrowserModule,
     FormsModule
   ],
-  declarations: [ 
+  declarations: [
     AppComponent,
     HeroDetailComponent
-    ],
-  bootstrap:    [ AppComponent ]
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
